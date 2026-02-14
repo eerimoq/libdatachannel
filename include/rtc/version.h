0df55ad6de01484b2d60b231f27dd8c65eb7b0c7
Erik Moqvist <erik.moqvist@gmail.com>
2026-02-14 11:11:14 +0100

    Version 0.25.0.

--- include/rtc/version.h
+++ include/rtc/version.h
@@ -2,8 +2,8 @@
 #define RTC_VERSION_H
 
 #define RTC_VERSION_MAJOR 0
-#define RTC_VERSION_MINOR 24
-#define RTC_VERSION_PATCH 1
-#define RTC_VERSION "0.24.1"
+#define RTC_VERSION_MINOR 25
+#define RTC_VERSION_PATCH 0
+#define RTC_VERSION "0.25.0"
 
 #endif
